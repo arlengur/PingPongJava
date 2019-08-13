7241f1ada33c7ffbf84c81f9c25da41fa517dd75
user <user@users-MacBook-Pro.local>
2019-08-13 19:03:17 +0300

    test added

---
+++ src/test/java/ru/arlen/ComparativeTest.java
@@ -0,0 +1,44 @@
+package ru.arlen;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 1)
+@State(value = Scope.Thread)
+@BenchmarkMode(Mode.SingleShotTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class ComparativeTest {
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(ComparativeTest.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public void pingPongSynch() {
+        new PingPongSync();
+    }
+
+    @Benchmark
+    public void pingPongAtomic() {
+        new PingPongAtomic();
+    }
+
+    @Benchmark
+    public void pingPongPark() {
+        new PingPongPark();
+    }
+
+    @Benchmark
+    public void pingPongLock() {
+        new PingPongLock();
+    }
+}
